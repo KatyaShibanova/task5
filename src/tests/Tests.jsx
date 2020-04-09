192fbaf8ce29d4e4ab4affc5126bfe15fc3d8e44
Katyusha <masiasha@mail.ru>
2020-04-09 16:40:05 +0300

    разметка для вопроса

--- src/tests/Tests.jsx
+++ src/tests/Tests.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 class Tests extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <h1>Hello</h1>;
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div><p class="font-weight-bold">Bold text.</p>
+        <div><p class="font-weight-normal">Normal weight text.</p>
+        <button type="button" id="1" class="btn btn-primary btn-lg btn-block">Block level button</button>
+        <button type="button" id="2" class="btn btn-primary btn-lg btn-block">Block level button</button>
+        <button type="button" id="3" class="btn btn-primary btn-lg btn-block">Block level button</button>
+        <button type="button" id="4" class="btn btn-primary btn-lg btn-block">Block level button</button>
+            </div>
+            </div>;
+    }
 }
 
 export default Tests;
