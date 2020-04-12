8dc706bfa91b715e53075570bd0c96cd57ca8952
Katyusha <masiasha@mail.ru>
2020-04-12 17:15:41 +0300

    слегка оптимизировала, что было

--- src/tests/Tests.jsx
+++ src/tests/Tests.jsx
@@ -5,20 +5,15 @@ class Tests extends React.Component {
         super(props);
         this.state = { score: 0 };
         this.Counter = this.Counter.bind(this);
-        this.CounterCorrect = this.CounterCorrect.bind(this);
         this.ButtonBlock = this.ButtonBlock.bind(this);
     }
 
     Counter() {
         this.setState(prevState => ({
-            score: prevState.score + 0
-        }))
-    }
-    CounterCorrect() {
-        this.setState(prevState => ({
             score: prevState.score + 1
         }))
     }
+    
     ButtonBlock() {
         this.refs.btn.setAttribute("disabled", "disabled");
     }
@@ -27,10 +22,10 @@ class Tests extends React.Component {
         return <div className="p-5">
             <div><p class="font-weight-bold">Q1</p>
                 <div><p class="font-weight-normal">Text</p>
-                    <button type="button" id="1" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
-                    <button ref="btn" type="button" id="2" class="btn btn-primary btn-lg btn-block" onClick={() => { this.CounterCorrect(); this.ButtonBlock()}}>Block level button</button>
-                    <button type="button" id="3" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
-                    <button type="button" id="4" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
+                    <button type="button" id="1" class="btn btn-primary btn-lg btn-block">Block level button</button>
+                    <button ref="btn" type="button" id="2" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter(); this.ButtonBlock()}}>Block level button</button>
+                    <button type="button" id="3" class="btn btn-primary btn-lg btn-block">Block level button</button>
+                    <button type="button" id="4" class="btn btn-primary btn-lg btn-block">Block level button</button>
                 </div>
             </div>
             <p class="font-weight-bold">Результат: {this.state.score}</p>
