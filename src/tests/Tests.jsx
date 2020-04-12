bcf5dc47def8b0b3938b34bf4f4bbedb8398e746
Katyusha <masiasha@mail.ru>
2020-04-12 04:14:16 +0300

    вывод результата по тесту

--- src/tests/Tests.jsx
+++ src/tests/Tests.jsx
@@ -3,24 +3,38 @@ import React from "react";
 class Tests extends React.Component {
     constructor(props) {
         super(props);
-        this.counter = this.counter.bind(this);
+        this.state = { score: 0 };
+        this.Counter = this.Counter.bind(this);
+        this.CounterCorrect = this.CounterCorrect.bind(this);
+        this.ButtonBlock = this.ButtonBlock.bind(this);
     }
+
+    Counter() {
+        this.setState(prevState => ({
+            score: prevState.score + 0
+        }))
+    }
+    CounterCorrect() {
+        this.setState(prevState => ({
+            score: prevState.score + 1
+        }))
+    }
+    ButtonBlock() {
+        this.refs.btn.setAttribute("disabled", "disabled");
+    }
+
     render() {
         return <div className="p-5">
-        <div><p class="font-weight-bold">Q1</p>
-        <div><p class="font-weight-normal">Text</p>
-        <button type="button" id="1" class="btn btn-primary btn-lg btn-block" onClick={()=>{this.counter()}}>Block level button</button>
-        <button type="button" id="2" class="btn btn-primary btn-lg btn-block" onClick={()=>{this.counter()}}>Block level button</button>
-        <button type="button" id="3" class="btn btn-primary btn-lg btn-block" onClick={()=>{this.counter()}}>Block level button</button>
-        <button type="button" id="4" class="btn btn-primary btn-lg btn-block" onClick={()=>{this.counter()}}>Block level button</button>
+            <div><p class="font-weight-bold">Q1</p>
+                <div><p class="font-weight-normal">Text</p>
+                    <button type="button" id="1" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
+                    <button ref="btn" type="button" id="2" class="btn btn-primary btn-lg btn-block" onClick={() => { this.CounterCorrect(); this.ButtonBlock()}}>Block level button</button>
+                    <button type="button" id="3" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
+                    <button type="button" id="4" class="btn btn-primary btn-lg btn-block" onClick={() => { this.Counter() }}>Block level button</button>
+                </div>
             </div>
-            </div>
-            </div>;
-    }
-    counter() {
-        // let score = 0;
-        // let count = this.props.button.id="2"?score+1:score;
-        // console.log(count);
+            <p class="font-weight-bold">Результат: {this.state.score}</p>
+        </div>;
     }
 }
 
